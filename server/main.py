794ad4492ba909d03e76381fdd4938555c6d83bb
Grigoriy <grishaskurihin1@gmail.com>
2023-04-15 21:35:18 +0300

    cummit

--- server/main.py
+++ server/main.py
@@ -1,36 +1,32 @@
-import configparser
 from flask import Flask, request, jsonify
 from PIL import Image, UnidentifiedImageError
 from waitress import serve
 from werkzeug.exceptions import *
-from model import Model
+from fastai.vision import *
+from fastai.vision.all import *
 
 app = Flask(__name__)
-config = configparser.ConfigParser()
-config.read('config.ini')
-model = Model(config["model"]["path"])
+model_path = "224_resnet50_unfreeze_da_dlrs_lra_mult.pkl"
+model = load_learner(model_path, cpu=True)
 
 @app.route('/', methods=['GET'])
 def main_route():
-    return jsonify({'message': "hello world"})
+    return jsonify({'message': "carification app"})
 
 @app.route('/predict_image', methods=['POST'])
 def process_image():
     try:
         file = request.files['image']
         img = Image.open(file.stream)
+        img = img.resize((299,299))
+        prediction,_,_ = model.predict(img)
+        return prediction
     except (BadRequest, KeyError):
         return jsonify({'error': "bad request"})
     except UnidentifiedImageError:
         return jsonify({'error': "file is not a valid image"})
     except Exception:
-        return jsonify({'error': "error on receiving and processing data"})
-    
-    try:
-        prediction = model.predict(img)
-        return jsonify({'prediction': prediction})
-    except Exception:
-        return jsonify({'error': "prediction error"})
+        return jsonify({'error': "error on prediction data"})
 
 if __name__ == '__main__':
-    serve(app, host=config["server"]["host"], port=config["server"]["port"])
+    serve(app, host="0.0.0.0", port=5000)
